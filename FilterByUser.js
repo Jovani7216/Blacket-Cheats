53b6f2f8dbc1e4084172af7b3f8f737300cf7299
Jovani7216 <131257546+Jovani7216@users.noreply.github.com>
2023-05-20 13:48:13 -0400

    Create FilterByUser.js
    To see who's username has blooks in the bazaar

---
+++ FilterByUser.js
@@ -0,0 +1,12 @@
+(async () => {
+  if (location.pathname !== '/bazaar') return alert('You must be on the Bazaar to run this script.');
+
+  let seller = prompt('Enter a name to filter...');
+
+  blacket.requests.get(`/worker/user/${seller}`, (data) => {
+    if (data.error) return alert('That\'s not a Blacket player.');
+    if (data.user.id === blacket.user.id) return alert('For your own Bazaar listings, just click the Listings button.');
+    
+    blacket.getBazaar(data.user.id);
+  });
+})();
